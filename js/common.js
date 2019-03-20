c3e1dcf57cb7379e445d4f41196ce8c75cfd1983
damienderoiste <48520438+damienderoiste@users.noreply.github.com>
2019-03-20 22:50:17 +0000

    updates

--- js/common.js
+++ js/common.js
@@ -25,29 +25,19 @@ $(document).ready(function () {
       $('#footer').html(data)
     }
   })
-});
+})
 
-function number_format(number, decimals, dec_point, thousands_sep) {
-  // *     example: number_format(1234.56, 2, ',', ' ');
-  // *     return: '1 234,56'
-  number = (number + '').replace(',', '').replace(' ', '');
-  var n = !isFinite(+number) ? 0 : +number,
-    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-    s = '',
-    toFixedFix = function(n, prec) {
-      var k = Math.pow(10, prec);
-      return '' + Math.round(n * k) / k;
-    };
-  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-  if (s[0].length > 3) {
-    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+app = {}
+app.common = {}
+
+app.common.thousandSeperator = function (int) {
+  int += ''
+  x = int.split('.')
+  x1 = x[0]
+  x2 = x.length > 1 ? '.' + x[1] : ''
+  var rgx = /(\d+)(\d{3})/
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2')
   }
-  if ((s[1] || '').length < prec) {
-    s[1] = s[1] || '';
-    s[1] += new Array(prec - s[1].length + 1).join('0');
-  }
-  return s.join(dec);
+  return x1 + x2
 }
--- population/PopulationChangeByCounty1841to2016/index.js
+++ population/PopulationChangeByCounty1841to2016/index.js
@@ -5,20 +5,84 @@ $(document).ready(function () {
     'https://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/EY001',
     function () {
       ds = this.Dataset(0)
-      displayResults(ds)
+      readCounties(ds)
+      $('#page-title').text(ds.label)
+      var time = ds.Dimension('Census Year').id
+      $('#bar-chart-card')
+        .find('.card-header')
+        .find('h6')
+        .text($('#county-select option:selected').text())
+
+      var ctx = document
+        .getElementById('population-by-county-bar-chart')
+        .getContext('2d')
+
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        responsive: true,
+        maintainAspectRatio: false,
+        data: getdata(ds, time),
+        options: {
+          tooltips: {
+            mode: 'label',
+            callbacks: {
+              title: function (tooltipItem, data) {
+                var total = ds.Data({
+                  Statistic: 'EY001C01',
+                  County: $('#county-select option:selected').val(),
+                  Sex: '-',
+                  'Census Year': tooltipItem[0].label
+                }).value
+                return (
+                  tooltipItem[0].label +
+                  ' Total: ' +
+                  app.common.thousandSeperator(total)
+                )
+              },
+
+              label: function (tooltipItem, data) {
+                return (
+                  data.datasets[tooltipItem.datasetIndex].label +
+                  ': ' +
+                  app.common.thousandSeperator(tooltipItem.yLabel)
+                )
+              }
+            }
+          },
+
+          scales: {
+            xAxes: [{ stacked: true }],
+            yAxes: [{ stacked: true }]
+          },
+          animation: {
+            onComplete: function () {
+              $('#county-select').change(function () {
+                $('#bar-chart-card')
+                  .find('.card-header')
+                  .find('h6')
+                  .text(
+                    $(this)
+                      .find('option:selected')
+                      .text()
+                  )
+                myChart.data = getdata(ds, time)
+                myChart.update()
+              })
+            }
+          }
+        }
+      })
+
+      $.preloader.stop()
     }
   )
 
-  $('#county-select').change(function () {
-    renderBarChart(ds)
-  })
-
   $('#county-select').select2({
-    theme: "classic"
-  });
+    theme: 'classic'
+  })
 })
 
-displayResults = function (ds) {
+readCounties = function (ds) {
   var counties = ds.Dimension('County').id
   $.each(counties, function (index, value) {
     $('#county-select').append(
@@ -29,19 +93,10 @@ displayResults = function (ds) {
     )
   })
 
-  renderBarChart(ds)
+  // renderData(ds)
 }
 
-renderBarChart = function (ds) {
-  $("#page-title").text(ds.label);
-  var time = ds.Dimension('Census Year').id
-  $('#bar-chart-card')
-    .find('.card-header')
-    .find('h6')
-    .text(
-      $('#county-select option:selected').text()
-    )
-
+getdata = function (ds, time) {
   var dataMales = []
   var dataFemales = []
 
@@ -83,43 +138,5 @@ renderBarChart = function (ds) {
     ]
   }
 
-  var ctx = document
-    .getElementById('population-by-county-bar-chart')
-    .getContext('2d')
-
-  ctx.height = 100;
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: {
-      tooltips: {
-        mode: 'label',
-        callbacks: {
-          title: function (tooltipItem, data) {
-            var total = ds.Data({
-              Statistic: 'EY001C01',
-              County: $('#county-select option:selected').val(),
-              Sex: '-',
-              'Census Year': tooltipItem[0].label
-            }).value
-            return tooltipItem[0].label + ' Total: ' + total
-          },
-
-          label: function (tooltipItem, data) {
-            return (
-              data.datasets[tooltipItem.datasetIndex].label +
-              ': ' +
-              tooltipItem.yLabel
-            )
-          }
-        }
-      },
-
-      scales: {
-        xAxes: [{ stacked: true }],
-        yAxes: [{ stacked: true }]
-      }
-    }
-  })
-  $.preloader.stop()
+  return data
 }
